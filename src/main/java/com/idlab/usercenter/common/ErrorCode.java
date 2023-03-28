9ef4a77fd1085c4d40dddb19022a95414ef59342
Emma <yaoxiuning@126.com>
2023-03-28 22:53:45 +0200

    增加错误码

---
+++ src/main/java/com/idlab/usercenter/common/ErrorCode.java
@@ -0,0 +1,36 @@
+package com.idlab.usercenter.common;
+
+/**
+ * 错误码
+ *
+ * @author Emma
+ */
+public enum ErrorCode {
+    SUCCESS(0, "ok", ""),
+    PARAMS_ERROR(40000, "请求参数错误", ""),
+    NULL_ERROR(40001, "请求数据为空", ""),
+    NOT_LOGIN(40100, "用户未登录", ""),
+    NO_AUTH(40101, "无权限", "");
+
+    private final int code;
+    private final String msg;
+    private final String description;
+
+    ErrorCode(int code, String msg, String description) {
+        this.code = code;
+        this.msg = msg;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
--- src/main/java/com/idlab/usercenter/common/ResultUtils.java
+++ src/main/java/com/idlab/usercenter/common/ResultUtils.java
@@ -1,7 +1,26 @@
 package com.idlab.usercenter.common;
 
+/**
+ * 返回结果封装工具
+ * @author Emma
+ */
 public class ResultUtils {
+    /**
+     * 成功
+     * @param data 结果数据
+     * @return 封装结果
+     * @param <T> 任意类型
+     */
     public static <T> BaseResponse<T> success(T data) {
         return new BaseResponse<>(0, data, "ok");
+    }
+
+    /**
+     * 失败
+     * @param errorCode 错误码
+     * @return 封装结果
+     */
+    public static BaseResponse error(ErrorCode errorCode) {
+        return new BaseResponse(errorCode);
     }
 }
